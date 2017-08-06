4bfbcc27e0a419e2108df53edc0c331f6f8d8c34
Turner M. Davis <turnermdavis@gmail.com>
2017-08-06 13:00:43 -0400

    refactor retrieve_jobs method to use location and language params, setup test for method sort_by_required_experience

--- GithubJobsApiConsumer/lib/github_jobs_search.rb
+++ GithubJobsApiConsumer/lib/github_jobs_search.rb
@@ -1,10 +1,13 @@
 require 'rest-client'
 module GithubJobsSearch
 
-  def self.retrieve_jobs(location, langauge="all")
-    url = 'https://jobs.github.com/positions.json?location=boston'
-    body = RestClient.get(url)
+  def self.retrieve_jobs(location, language)
+    url = "https://jobs.github.com/positions.json?location=#{location}&description=#{language}"
+    body = RestClient::Request.execute(:method => :get, :url => url, :timeout => 10, :open_timeout => 10)
     JSON.parse(body)
   end
 
+  def self.sort_by_required_experience(jobs, min_years)
+    
+  end
 end
--- GithubJobsApiConsumer/test/lib/github_jobs_search_test.rb
+++ GithubJobsApiConsumer/test/lib/github_jobs_search_test.rb
@@ -3,15 +3,19 @@ require 'minitest/pride'
 
 class GithubJobsSearchTest < ActiveSupport::TestCase
   setup do
+    @job_data = GithubJobsSearch.retrieve_jobs("san+fransisco","python")
   end
 
   teardown do
   end
 
   should "return data" do
-    data = GithubJobsSearch.retrieve_jobs("boston");
-    p data
-    assert data.present?
+    assert @job_data.present?
+  end
+
+  should "return return 4 positions for 5+ years of experience" do
+    sorted = GithubJobsSearch.sort_by_required_experience(@job_data,5)
+    assert sorted.count == 4
   end
 
 end
