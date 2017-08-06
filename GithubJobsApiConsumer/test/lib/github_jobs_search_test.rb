5cc188053115941b641fde5be7d47b1183afbc8b
Turner M. Davis <turnermdavis@gmail.com>
2017-08-06 13:38:48 -0400

    fix test titles

--- GithubJobsApiConsumer/test/lib/github_jobs_search_test.rb
+++ GithubJobsApiConsumer/test/lib/github_jobs_search_test.rb
@@ -14,17 +14,17 @@ class GithubJobsSearchTest < ActiveSupport::TestCase
     assert @job_data.present?
   end
 
-  should "return return 4 positions for 5+ years of experience" do
+  should "return return 4 positions for min 5 years of experience" do
     sorted = GithubJobsSearch.sort_by_required_experience(@job_data,5)
     assert sorted.count == 4
   end
 
-  should "return return 4 positions for 5+ years of experience" do
+  should "return return 4 positions for min 3 years of experience" do
     sorted = GithubJobsSearch.sort_by_required_experience(@job_data,3)
     assert sorted.count == 3
   end
 
-  should "return return 4 positions for 5+ years of experience" do
+  should "return return 4 positions for min 0 years of experience" do
     sorted = GithubJobsSearch.sort_by_required_experience(@job_data,0)
     assert sorted.count == 0
   end
